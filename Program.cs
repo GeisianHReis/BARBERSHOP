5e46792c4fea020c9b0c8dc2aeca5f4f723af374
Geisian Reis <geisian.reis@levva.io>
2024-11-02 12:13:05 -0300

    Update Entity Framework Core to version 8.0.10 and remove unused namespaces in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 
